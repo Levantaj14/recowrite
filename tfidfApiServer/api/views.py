ed6b87d635a792ee63078f6b824893b932068a8a
Levente Szabo <levicske12@gmail.com>
2025-02-22 02:04:16 +0200

    Updated the way it handles database connections

--- tfidfApiServer/api/views.py
+++ tfidfApiServer/api/views.py
@@ -5,8 +5,6 @@ from tfidf import tf_idf
 from utils import get_db_handle
 from .serializers import ListSerializer, BlogTitleSerializer
 
-db = get_db_handle()
-
 
 @api_view(['GET'])
 def blog_list(request):
@@ -14,16 +12,18 @@ def blog_list(request):
     k = int(request.GET.get('k', 3))
 
     if blog_id.isnumeric():
-        mycursor = db.cursor()
-        mycursor.execute("SELECT * FROM blogs WHERE id = %s", [blog_id])
-        result = mycursor.fetchone()
+        with get_db_handle() as connection:
+            with connection.cursor() as mycursor:
+                mycursor.execute("SELECT * FROM blogs WHERE id = %s", [blog_id])
+                result = mycursor.fetchone()
+        
         if result:
             data = tf_idf.search(result[2], k)
             serializer = ListSerializer(data={'data': data})
             if serializer.is_valid():
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response('', status=status.HTTP_200_OK)
+        return Response('{}', status=status.HTTP_200_OK)
     return Response('ID must be an integer greater than 0', status=status.HTTP_400_BAD_REQUEST)
 
 
