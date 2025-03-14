969629c4c62d84cdf4779f18f37e980a311d1066
Levente Szabo <levicske12@gmail.com>
2025-03-14 20:04:35 +0200

    The login page won't load when someone is logged in

--- frontend/src/components/pages/login/LoginPage.tsx
+++ frontend/src/components/pages/login/LoginPage.tsx
@@ -3,9 +3,21 @@ import { motion } from 'framer-motion';
 import Login from '@/components/pages/login/Login.tsx';
 import SignUp from '@/components/pages/login/SignUp.tsx';
 import { useTranslation } from 'react-i18next';
+import { UserDetailContext } from '@/contexts/userDetailContext.ts';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
 function LoginPage() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const {userDetails} = useContext(UserDetailContext);
+
+  useEffect(() => {
+    if (userDetails !== null) {
+      navigate('/');
+    }
+  }, [navigate, userDetails])
+
   return (
     <Tabs.Root lazyMount unmountOnExit defaultValue="login">
       <Tabs.List>
