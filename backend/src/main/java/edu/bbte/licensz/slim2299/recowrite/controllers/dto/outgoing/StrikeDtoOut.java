3cc1ea392def9abd1b1fae901931c1b2fda9ae2c
Levente Szabo <levicske12@gmail.com>
2025-06-09 02:13:26 +0300

    Removed unused import

--- backend/src/main/java/edu/bbte/licensz/slim2299/recowrite/controllers/dto/outgoing/StrikeDtoOut.java
+++ backend/src/main/java/edu/bbte/licensz/slim2299/recowrite/controllers/dto/outgoing/StrikeDtoOut.java
@@ -3,8 +3,6 @@ package edu.bbte.licensz.slim2299.recowrite.controllers.dto.outgoing;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Date;
-
 @AllArgsConstructor
 @Data
 public class StrikeDtoOut {
