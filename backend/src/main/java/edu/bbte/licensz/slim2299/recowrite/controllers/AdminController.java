f8f18d16bd70a0c4b80163703b0673b60f9aa4bd
Levente Szabo <levicske12@gmail.com>
2025-06-08 16:40:40 +0300

    Removed the strike API endpoints

--- backend/src/main/java/edu/bbte/licensz/slim2299/recowrite/controllers/AdminController.java
+++ backend/src/main/java/edu/bbte/licensz/slim2299/recowrite/controllers/AdminController.java
@@ -1,15 +1,9 @@
 package edu.bbte.licensz.slim2299.recowrite.controllers;
 
-import edu.bbte.licensz.slim2299.recowrite.config.JwtUtil;
-import edu.bbte.licensz.slim2299.recowrite.controllers.dto.incoming.StrikeDtoIn;
 import edu.bbte.licensz.slim2299.recowrite.controllers.dto.outgoing.*;
 import edu.bbte.licensz.slim2299.recowrite.services.AccountServiceInterface;
 import edu.bbte.licensz.slim2299.recowrite.services.ReportServiceInterface;
-import edu.bbte.licensz.slim2299.recowrite.services.StrikeServiceInterface;
 import edu.bbte.licensz.slim2299.recowrite.services.UserServiceInterface;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,54 +15,19 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminController {
     private final ReportServiceInterface reportService;
-    private final StrikeServiceInterface strikeService;
     private final AccountServiceInterface accountService;
     private final UserServiceInterface userService;
-    private final AuthCookieFinder authCookieFinder;
-    private final JwtUtil jwtUtil;
-
     @Autowired
-    public AdminController(ReportServiceInterface reportService, StrikeServiceInterface strikeService,
-                           AccountServiceInterface accountService, UserServiceInterface userService,
-                           AuthCookieFinder authCookieFinder, JwtUtil jwtUtil) {
+    public AdminController(ReportServiceInterface reportService, AccountServiceInterface accountService,
+                           UserServiceInterface userService) {
         this.reportService = reportService;
-        this.strikeService = strikeService;
         this.accountService = accountService;
         this.userService = userService;
-        this.authCookieFinder = authCookieFinder;
-        this.jwtUtil = jwtUtil;
     }
 
     @GetMapping()
     public ResponseEntity<MessageDtoOut> test() {
         return ResponseEntity.status(HttpStatus.OK).body(new MessageDtoOut("Admin endpoint accessible"));
-    }
-
-    @GetMapping("/strikes")
-    public ResponseEntity<List<StrikeDtoOut>> getStrikes() {
-        return ResponseEntity.ok(strikeService.getAllStrikes());
-    }
-
-    @PostMapping("/strikes")
-    public ResponseEntity<IdDtoOut> addStrike(HttpServletRequest request, @RequestBody @Valid StrikeDtoIn strikeDtoIn) {
-        Cookie cookie = authCookieFinder.serachAuthCookie(request.getCookies());
-        if (cookie != null) {
-            long response = strikeService.addStrike(jwtUtil.extractUsername(cookie.getValue()), strikeDtoIn);
-            return ResponseEntity.ok(new IdDtoOut(response));
-        }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-    }
-
-    @DeleteMapping("/strikes/{id}")
-    public ResponseEntity<MessageDtoOut> deleteStrike(@PathVariable("id") long id) {
-        strikeService.deleteStrike(id);
-        return ResponseEntity.status(HttpStatus.OK).body(new MessageDtoOut("Strike revoked successfully"));
-    }
-
-    @DeleteMapping("strikes/report/{id}")
-    public ResponseEntity<MessageDtoOut> deleteStrikeFromReport(@PathVariable("id") long id) {
-        strikeService.deleteStrikeFromReport(id);
-        return ResponseEntity.status(HttpStatus.OK).body(new MessageDtoOut("Strike removed successfully"));
     }
 
     @GetMapping("/reports")
